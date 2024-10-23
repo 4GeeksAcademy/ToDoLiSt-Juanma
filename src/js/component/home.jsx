28c3f321fa62c454bea230db9c9a5c99a58c7768
Bertitom <juanbertoletti2@gmail.com>
2024-10-23 22:11:18 +0000

    New Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,51 +1,128 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	let [listaDeTareas, setListaDeTareas] = useState(["bañarse", "limpiar", "cocinar", "aprender react"]);
-	const [nuevaTarea, setNuevaTarea] = useState("")
+	const [tarea, setTarea] = useState("")
+	const [lista, setLista] = useState([])
+
+	async function agregarTareas(evento) {
+		evento.preventDefault()
+		if (tarea != "") {
+			guardarTareas()
+			setTarea("")
+		} else {
+			alert("Ingresar Tarea")
+		}
+		if (evento.key === "Enter") {
+			guardarTareas()
+			setTarea("")
+		}
+	}
+
+	const guardarTareas = async () => {
+		try {
+			const url = "https://playground.4geeks.com/todo/todos/juanma"
+			const resp = await fetch(url, {
+				method: "POST",
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					label: tarea,
+					is_done: false
+				})
+			})
+			if (resp.ok) {
+				cargarTareas()
+				return true
+			}
+		} catch (error) {
+			console.log(error)
+			return false
+		}
+	}
+	const cargarTareas = async () => {
+		try {
+			const url = "https://playground.4geeks.com/todo/users/juanma"
+			const resp = await fetch(url)
+			if (resp.status == 404) {
+				crearUsuario()
+				return
+			}
+			const data = await resp.json()
+			setLista(data.todos)
+			return true
+		} catch (error) {
+			console.error(error)
+			return false
+		}
+	}
+
+	const crearUsuario = async () => {
+		try {
+			const resp = await fetch("https://playground.4geeks.com/todo/users/juanma", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" }
+			})
+			if (resp.status == 201) {
+				cargarTareas()
+			}
+		} catch (error) {
+			console.log(error)
+			return false
+		}
+	}
+
+	const borrar = async (id) => {
+		// console.log(id)
+		try {
+			const resp = await fetch("https://playground.4geeks.com/todo/todos/" + id, {
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" }
+			})
+			// console.log(resp.status)
+			if (resp.status == 204) {
+				cargarTareas()
+				return true
+			}
+		} catch (error) {
+			console.log(error)
+			return false
+		}
+	}
+
+	useEffect(() => {
+		cargarTareas()
+	}, [])
+
 	return (
-		<div id="ppal" className="container mt-5 w-75 mx-auto">
-			<h1 className="text-center mt-5">todos </h1>
-			<div className="col-6 mx-auto">
-				<input type="text" className="form-control" placeholder="¿Qué tarea quieres hacer?"
-					value={nuevaTarea} onChange={(evento) => {
-						setNuevaTarea(evento.target.value)
-					}}
-					onKeyUp={(evento) => {
-						if (evento.key == "Enter") {
-							setListaDeTareas([...listaDeTareas, nuevaTarea]);
-							setNuevaTarea("")
-
-						}
-						//	console.log(evento.key)
-					}}
-
-				/>
-				<ul className="list-group">
-					{listaDeTareas.map((item, index) => {
-						return (
-							<li className="list-group-item" key={index}>
-								{item}<i onClick={() => {
-									const aux = listaDeTareas.filter((_task, ind) => {
-										return (ind != index)
-									})
-									setListaDeTareas(aux)
-								}}
-									className="fa-solid fa-trash icono-oculto"></i>
-							</li>
-						)
-					})}
-				</ul>
-				<hr className="border border-secondary border-1 opacity-30"></hr>
-				<span className="text-secondary-emphasis">
-					{listaDeTareas.length} items left
-				</span><br />
-				<span className="text-secondary-emphasis">
-					{(listaDeTareas.length == 0) ? "No hay tareas, agrega una" : ""}
-				</span>
-			</div>
+		<div className="container text-center ">
+			<h1 className="text-center mt-5 ">Todos</h1>
+			<form className="row">
+				<div className="col-12 ">
+					<input type="text "
+						//  onKeyDown={(e) => agregarTareas(e)} 
+						className="form-control border border-success" placeholder="Nueva Tarea" value={tarea} onChange={(evento) => setTarea(evento.target.value)} />
+				</div>
+				<div className="col-12 mt-2">
+					<button type="submit" onClick={(evento) => agregarTareas(evento)} className="btn btn-success mb-3">Agregar Tarea</button>
+				</div>
+			</form>
+			<ul className="list-group border-success">
+				{lista.map((item, index) => (
+					<li className="list-group-item border border-success" key={index}>
+						{item.label}
+						<i onClick={() => borrar(item.id)} className="m-1 fa-solid fa-trash icono-oculto"></i>
+					</li>
+				))}
+			</ul>
+			<span className="text-success">
+				{(lista.length == 0) ? "No hay Tareas , Agregar Una " : ""}
+			</span>
+			<p></p>
+			<span className="text-success">{lista.length} Items Left</span>
 		</div>
 	);
 };
